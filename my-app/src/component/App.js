f773eb42280e8480cf86d30c317a58273bae3da3
WenakiTan <k9162477@icloud.com>
2020-03-30 12:16:02 -0400

    Update App.js

--- my-app/src/component/App.js
+++ my-app/src/component/App.js
@@ -10,31 +10,25 @@ import QuestionPage from './question'
 import NotFound from './notFound'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import LoadingBar from 'react-redux-loading'
- import PrivateRoute from './privateRoute'
+import PrivateRoute from './privateRoute'
 class App extends Component {
   componentDidMount(){
     this.props.dispatch(handleInitialDataAction())
   }
 
   render(){
-    const { isAuthenticated } = this.props
-
     return (
       <Router>
         <Fragment>
           <LoadingBar />
+          <NavBar />
           <Switch>
-            {isAuthenticated ? 
-            <Fragment>
-              <NavBar />
-              <Route path='/' exact component={Dashboard} />
+              <Route path='/login' exact component={SignIn} />
+              <PrivateRoute path='/' exact component={Dashboard} />
               <PrivateRoute path='/leaderboard' component={LeaderBoard} />
-              <PrivateRoute path='/new' component={NewQuestion} />
+              <PrivateRoute path='/add' component={NewQuestion} />
               <PrivateRoute path='/questions/:id' component={QuestionPage} />
-            </Fragment>
-              : <Route path='/' exact component={SignIn} />
-            }
-            <PrivateRoute component={NotFound} />
+              <PrivateRoute component={NotFound} />
           </Switch>
         </Fragment>
       </Router>
