3c4a43c18b4547089bf9a0e7ffd2ce6df2af77de
WenakiTan <k9162477@icloud.com>
2020-03-30 11:30:51 -0400

    Update nav.js

--- my-app/src/component/nav.js
+++ my-app/src/component/nav.js
@@ -14,52 +14,61 @@ import {
 import { connect } from 'react-redux'
 import {Link} from 'react-router-dom'
 import { unsetAuthedUser } from '../action/authedUser';
-const NavBar = (props) => {
-    const [isOpen, setIsOpen] = useState(false);
+const NavBar = props => {
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggle = () => setIsOpen(!isOpen);
-    const { user } = props
-    const handleClick = (e) => {
-        props.dispatch(unsetAuthedUser())
-    }
-        return (
-            
-            <div>
-                <>{user &&                 
-                <Container>
-                    <Navbar bg='success' variant='dark' light expand="md">
-                        <NavbarToggler onClick={toggle} />
-                        <Collapse isOpen={isOpen} navbar>
-                            <Nav className="mr-auto" navbar >
-                                <NavbarBrand className='n h1'>Would you rather</NavbarBrand>
-                                <NavItem>
-                                    <NavLink tag={Link} to="/" >Home</NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink tag={Link} to="/add">New Question</NavLink>
-                                </NavItem>
-                                <NavItem>
-                                    <NavLink tag={Link} to="/leaderboard">Leader Board</NavLink>
-                                </NavItem>
-                            </Nav>
+  const toggle = () => setIsOpen(!isOpen);
+  const { user } = props;
+  const handleClick = e => {
+    props.dispatch(unsetAuthedUser());
+  };
+  return (
+    <>
+      {user && (
+        <div>
+          <Container>
+            <Navbar bg="success" variant="dark" light expand="md">
+              <NavbarToggler onClick={toggle} />
+              <Collapse isOpen={isOpen} navbar>
+                <Nav className="mr-auto" navbar>
+                  <NavbarBrand className="n h1">Would you rather</NavbarBrand>
+                  <NavItem>
+                    <NavLink tag={Link} to="/">
+                      Home
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink tag={Link} to="/add">
+                      New Question
+                    </NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink tag={Link} to="/leaderboard">
+                      Leader Board
+                    </NavLink>
+                  </NavItem>
+                </Nav>
 
-                            <NavbarText className='text-right'>
-                                <img src={user.avatarURL} className='avatar' alt={`Avatar of ${user.name}`} />
-                                <span className='b'>Hello {user.name}</span>
-                            </NavbarText>
+                <NavbarText className="text-right">
+                  <img
+                    src={user.avatarURL}
+                    className="avatar"
+                    alt={`Avatar of ${user.name}`}
+                  />
+                  <span className="b">Hello {user.name}</span>
+                </NavbarText>
 
-                            <Button outline color='seconddary' onClick={handleClick} >Log out</Button>
-
-
-                        </Collapse>
-                    </Navbar>
-                </Container>}</>
-
-
-            </div>
-        );
-    
-}
+                <Button outline color="seconddary" onClick={handleClick}>
+                  Log out
+                </Button>
+              </Collapse>
+            </Navbar>
+          </Container>
+        </div>
+      )}
+    </>
+  );
+};
 
 function mapStateToProps({users,authedUser}){
     return{
